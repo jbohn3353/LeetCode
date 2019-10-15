6b9af82b06577a437392b60a73b7095ab5bbaa76
jbohn3353 <jbohn3353@gmail.com>
2019-10-14 22:35:58 -0600

    Problem 1. Two Sum complete (time and space efficient)

--- LeetCode/1. Two Sum/1. Two Sum.cpp
+++ LeetCode/1. Two Sum/1. Two Sum.cpp
@@ -1,20 +1,37 @@
-// 1. Two Sum.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* James Bohn
+ * 10/14/19
+ * Two solid solutions for LeetCode 1. Two Sum. One solution is time efficient,
+ * the other is space efficient.
+ */
 
 #include <iostream>
+#include <unordered_map>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class Solution {
+public:
+	//time efficient solution (one pass)
+	vector<int> twoSumTime(vector<int>& nums, int target) {
+		unordered_map<int, int> priorNumsMap;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+		for (size_t i = 0; i < nums.size(); ++i) {
+			auto possibleIndexOfPair = priorNumsMap.find(target - nums[i]);
+			if (possibleIndexOfPair != priorNumsMap.end()) {
+				return vector<int> { possibleIndexOfPair->second, int(i) };
+			}
+			priorNumsMap.insert({ nums[i], i });
+		}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+		return vector<int>();
+	}
+
+	//space efficient solution
+	vector<int> twoSumSpace(vector<int>& nums, int target) {
+		for (int i = 0; i < nums.size(); ++i) {
+			for (int j = i+1; j < nums.size(); ++j) {
+				if (nums[i] + nums[j] == target) return vector<int> {i, j};
+			}
+		}
+		return vector<int>();
+	}
+};
